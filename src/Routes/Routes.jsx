ec1c8a14a7ac2648731a15f9a321b47bedbbfd09
dronatoo <mrdronatoo@gmail.com>
2023-11-07 22:05:06 -0800

    added error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,11 +5,13 @@ import Services from "../Pages/Services/Services";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import AddService from "../Pages/AddService/AddService";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root/>,
+        errorElement: <ErrorPage/>,
         children: [
             {
                 path: '/',
