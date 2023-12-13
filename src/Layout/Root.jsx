f5513d54e82b859b82ab91bcf18b3d8a660ece2d
dronatoo <mrdronatoo@gmail.com>
2023-12-13 11:03:45 -0800

    added messenger chat bot

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -3,6 +3,7 @@ import Navbar from '../Pages/Shared/Navbar/Navbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from '../Pages/Shared/Footer/Footer';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const Root = () => {
     return (
@@ -11,6 +12,10 @@ const Root = () => {
             <Outlet/> 
             <Footer/>
             <ToastContainer/> 
+            <MessengerCustomerChat
+               pageId="191736287353424"
+               appId="657363919934309"
+             />,
         </div>
     );
 };
